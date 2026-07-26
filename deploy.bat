025cf5da45c3e21a7eca2f49dacbcbdb0cf3ef19
JackeryTU <1229203790@qq.com>
2026-07-26 09:37:00 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -4,10 +4,10 @@ git add .
 git commit -m "update"
 git push
 
-echo 开始构建并推送到 Gitee (使用独立配置)...
+echo 开始构建并推送到 GitHub Pages (使用独立配置)...
 call hexo clean
-call hexo generate --config _config.yml,_config.gitee.yml
-call hexo deploy --config _config.yml,_config.gitee.yml
+call hexo generate --config _config.yml,_config.github.yml
+call hexo deploy --config _config.yml,_config.github.yml
 
-echo 全部完成！记得去 Gitee 仓库「服务 -^> Gitee Pages」点一下「更新」按钮
+echo 全部完成！
 pause
